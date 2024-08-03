c089ad199da2f163871b021d6ccdfb4085f4bc24
kprieto <anakarenprieto@live.com>
2024-08-02 19:15:10 -0700

    Update juegoDelAhorcado.py

--- juegoDelAhorcado.py
+++ juegoDelAhorcado.py
@@ -6,7 +6,7 @@ import time
 
 
 def bienvenidos():
-    print("Bienvenidos al juego del ahorcado")
+    print("Bienvenidos al Juego Del Ahorcado!!!")
 
 def random_palabra():
     list_palabras =["zapato","casa","árbol","computadora"]
